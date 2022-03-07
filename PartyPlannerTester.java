6b1ef12198eff6f170ae8994705fa2000fee5fcd
hartmmac22 <59925490+hartmmac22@users.noreply.github.com>
2022-03-07 11:04:28 -0500

    Create PartyPlannerTester.java

---
+++ PartyPlannerTester.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PartyPlannerTester {
+	/*
+	try {
+		File myObj = new File("people.csv");
+		Scanner scan = new Scanner(myObj);
+	}
+	
+	catch (FileNotFoundException e){
+		System.out.println("error");
+		e.printStackTrace();
+	}	
+	*/
+	PartyPlanner m1 = new PartyPlanner();
+	m1.People();
+}
