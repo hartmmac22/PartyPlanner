03f3c1eb6604d48492e4d2fe0ead8d4000f3d4b8
hartmmac22 <59925490+hartmmac22@users.noreply.github.com>
2022-03-09 11:05:45 -0500

    Update PartyTester.java

--- PartyTester.java
+++ PartyTester.java
@@ -15,15 +15,93 @@ public class PartyTester {
 			person.add(temp);
 		}
 		
-		//begining of algorithm for sorting tables
-		System.out.print(person);
+		//sorting algorithm
 		int x = 0;
+		int companyOne = 0;
+		int companyTwo = 0;
+		int companyThree = 0;
+		int companyFour = 0;
+		int companyFive = 0;
+		
+		//this does basic sorting for tables for 10 people
 		for (int i = 0; i < 10; i++){
-			if (x == 0) {
-				People temp = new People(tableID[i], name[i], 1);
-				person.set(i, temp);
+			if (person.get(i).companyID == 1) {
+				if(companyOne != 1){
+					People temp = new People(1, name[i], companyID[i]);
+					person.set(i, temp);
+					companyOne = 1;
+				}
+				else {
+					People temp = new People(2, name[i], companyID[i]);
+					person.set(i, temp);
+				}
+			}
+			
+			if (person.get(i).companyID == 2) {
+				if(companyTwo != 1){
+					People temp = new People(1, name[i], companyID[i]);
+					person.set(i, temp);
+					companyTwo = 1;
+				}
+				else {
+					People temp = new People(2, name[i], companyID[i]);
+					person.set(i, temp);
+				}
+			}
+			
+			if (person.get(i).companyID == 3) {
+				if(companyThree != 1){
+					People temp = new People(1, name[i], companyID[i]);
+					person.set(i, temp);
+					companyThree = 1;
+				}
+				else {
+					People temp = new People(2, name[i], companyID[i]);
+					person.set(i, temp);
+				}
+			}
+			
+			if (person.get(i).companyID == 4) {
+				if(companyFour != 1){
+					People temp = new People(1, name[i], companyID[i]);
+					person.set(i, temp);
+					companyFour = 1;
+				}
+				else {
+					People temp = new People(2, name[i], companyID[i]);
+					person.set(i, temp);
+				}
+			}
+			
+			if (person.get(i).companyID == 5) {
+				if(companyFive != 1){
+					People temp = new People(1, name[i], companyID[i]);
+					person.set(i, temp);
+					companyFive = 1;
+				}
+				else {
+					People temp = new People(2, name[i], companyID[i]);
+					person.set(i, temp);
+				}
 			}
 		}
+		System.out.println(person);
 		
+		//this prints out who's assigned to which table
+		System.out.println("\n" + "Table 1 : ");
+		for (int i = 0; i < person.size(); i++) {
+			if (person.get(i).tableID == 1) {
+				System.out.println("Name: " + person.get(i).name + ", Company " + person.get(i).companyID);
+			}
+		}
+			
+		System.out.println("\n" + "Table 2 : ");
+		for (int i = 0; i < person.size(); i++) {
+			if (person.get(i).tableID == 2) {
+				System.out.println("Name: " + person.get(i).name + ", Company " + person.get(i).companyID);
+			}
+		}
+			
+			
 	}
 }
