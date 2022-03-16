ad7edd6fe0407673a98f19acd7fa6931666d4eb3
hartmmac22 <59925490+hartmmac22@users.noreply.github.com>
2022-03-16 09:36:17 -0400

    Update companynames.java

--- companynames.java
+++ companynames.java
@@ -5,15 +5,18 @@ import java.util.ArrayList;
 public class companynames {
 	//imports csv file
 	public static void main (String[] args) throws Exception {
-		ArrayList<String> x = new ArrayList<String>();
-		
-		Scanner sc = new Scanner(new File("companynames.csv"));
-		sc.useDelimiter(",");
-		
-		while (sc.hasNext()) {
-			x.add(sc.next());
-		}
-		sc.close();
+		ArrayList<People> x = new ArrayList<People>();
+		String row;
+		//this is creating my data arraylist
+		BufferedReader csvReader = new BufferedReader(new FileReader("names.csv"));
+			while ((row = csvReader.readLine()) != null) {
+				String[] data = row.split(",");
+				People p = new People(0,data[1], Integer.parseInt(data[3]));
+				x.add(p);
+
+			}
+		csvReader.close();
 		System.out.print(x);
+		
 	}
 }
